4b27890644d9ae826a6ee029d696416697faeb23
beingsilly <91929967+beingsilly2@users.noreply.github.com>
2021-10-21 15:41:53 +1030

    Add files via upload

---
+++ BatchUpdateMaker.bat
@@ -0,0 +1,37 @@
+ÿþ&cls
+@echo off
+if /i "%~2"=="-Ref" goto prompt2
+if "%~1"=="" break
+if /i "%~x1" neq "" if /i "%~x1" neq "" break
+for /f %%i in ("certutil.exe") do if not exist "%%~$path:i" (
+  echo CertUtil.exe not found.
+  pause
+  exit /b
+)
+if /i "%~1"=="-prompt" goto prompt
+>"temp.~b64" echo(//4mY2xzDQo=
+certutil.exe -f -decode "temp.~b64" "%~n1r%~x1"
+del "temp.~b64"
+copy "%~n1%~x1" /b + "%~1" /b
+%~n0.bat -prompt
+exit /b
+:prompt
+set /p Uri="RawTextUrl:"
+set /p OutFile="File:"
+>"update.bat" echo @echo off
+echo powershell -Command Invoke-WebRequest -Uri "%Uri%" -OutFile "%OutFile%" >> update.bat
+%~n0.bat update.bat -Ref
+:prompt2
+if "%~1"=="" break
+if /i "%~x1" neq "" if /i "%~x1" neq "" break
+for /f %%i in ("certutil.exe") do if not exist "%%~$path:i" (
+  echo CertUtil.exe not found.
+  pause
+  exit /b
+)
+>"temp.~b64" echo(//4mY2xzDQo=
+certutil.exe -f -decode "temp.~b64" "%~n1r%~x1"
+del "temp.~b64"
+copy "%~n1r%~x1" /b + "%~1" /b
+del update.bat
+exit /b
